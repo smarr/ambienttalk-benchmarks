14300a0e778d8c605487d07dde7fd028f49667a4
Stefan Marr <git@stefan-marr.de>
2013-04-05 01:08:39 +0200

    First set of changes to make it all useable on other machines.

--- src/at/bench/Constants.java
+++ src/at/bench/Constants.java
@@ -8,14 +8,14 @@ public interface Constants {
 	public static final String _ENV_AT_STACKSIZE_ = "524288";
 	
 	// Mount point of the 'primary media directory'
-	public static final File   _ENV_AT_BASE_ = new File("/");
+	public static final File   _ENV_AT_BASE_ = new File(System.getProperty("user.dir"));
 	
 	// Folder under which the assets can be found (see AssetInstaller).
 	public static final String _ENV_AT_ASSETS_BASE_ = "atlib";
 	
 	// Relative paths to at-home 
-	public static final String _AT_HOME_RELATIVE_PATH_ = "/Users/smarr/Projects/PostDoc/AliasMOP/at2dist060213/atlib";
-	public static final String _AT_TEMP_FILES_PATH = "/Users/smarr/Projects/PostDoc/AliasMOP/at2dist060213/atlib/cache/";
+	public static final String _AT_HOME_RELATIVE_PATH_ = "lib/atlib";
+	public static final String _AT_TEMP_FILES_PATH     = "/lib/atlib/cache/";
 	
 	// Result code when AT installation fails
 	// public static final int _RESULT_FAIL_ = Activity.RESULT_FIRST_USER;
